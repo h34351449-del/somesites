cd47b017b4379188c18bea96470004c058dc644e
Void Coder <h34351449@gmail.com>
2026-08-21 12:02:59 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#if you don't have the required modules
+#first make it executable run chmod +x setup.sh
+#then run it ./setup.sh
 echo "Installing somesites..."
 pip install beautifulsoup4 requests --break-system-packages
 chmod +x $PREFIX/bin/somesites
 echo "Installation complete! Type 'somesites' to run."
-#Then type chmod +x setup.sh
